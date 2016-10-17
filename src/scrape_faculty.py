bd2cc013fe8ec233a189a3e8355fd9d6cc24de84
Tom Davidson <trd54@cornell.edu>
2016-10-16 23:26:39 -0400

    Added docstrings to functions

--- src/scrape_faculty.py
+++ src/scrape_faculty.py
@@ -4,12 +4,24 @@ from bs4 import BeautifulSoup as BS
 from time import sleep
 
 def getSoup(url):
+    """Input: A valid url.
+    Gets the HTML associated with a url and
+    converts it to a BeautifulSoup object.
+    Then sleeps for a short time.
+    Returns: A BeautifulSoup object."""
     html = requests.get(url)
     soup = BS(html.text, "html.parser")
     sleep(0.25)
     return soup
 
 def getFacultyPages(base_url):
+    """Input: The base url for the
+    Cornell sociology faculty site.
+    Finds all valid links to faculty profiles
+    then visits each link and gets the soup
+    object.
+    Returns: An iterator of faculty profiles
+    soup objects"""
     soup = getSoup(base_url)
     links = soup.findAll('a', href=True)
     profiles = []
@@ -22,10 +34,20 @@ def getFacultyPages(base_url):
         yield getSoup(p)
 
 def getFacultyInfo(soup):
+    """Input: A soup object related to a faculty webpage.
+    Finds the section of the page with the class
+    entry-content, based on inspection of relevant pages,
+    Returns: A soup object with the relevant content."""
     info = soup.find('div', {'class': 'entry-content'})
     return info
 
 def getTitleAndEducation(info):
+    """Input: A soup object output by getFacultyInfo()
+    that contains information on a faculty member.
+    This function parses this object to get the title
+    and education for a given faculty member.
+    Reutrns: A tuple containing title and education
+    strings."""
     info_refined = info.findAll('h4')
     titles = info_refined[0].text
     title_and_education = titles.split('PhD')
@@ -36,6 +58,9 @@ def getTitleAndEducation(info):
     return title, education
 
 def getFacultyName(soup):
+    """Input: The soup object for a faculty page.
+    Gets the faculty members name as it appears on the page.
+    Returns: The name as a string."""
     name_info = soup.findAll('h1', {'class':'entry-title'})
     name = name_info[0].text
     return name
